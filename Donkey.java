c5431b6700239c1aad8ea184cde687812c2022a9
Amit Shah <amit2054128@gmail.com>
2026-06-05 09:54:01 +0545

    fix bug

---
+++ Donkey.java
@@ -0,0 +1,19 @@
+class Donkey{
+/*  main method
+    Entry point of java
+ */
+    //token
+    //keywords contextual keyword
+    //literals - fix value 
+
+public static void main(String[] args) {
+
+    
+   int a = 1;
+   type identifier assing value
+   String stuzdent_name = 1;
+    System.out.println("hello world");
+}
+}
+
+
---
+++ Operator.java
@@ -0,0 +1,6 @@
+public class Operator{
+    public static void main(String[] args) {
+
+        double a = 3.0;
+    }
+}
